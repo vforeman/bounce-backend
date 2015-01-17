70eb5b9c5f06c30fba830fbb63d968799c50427e
Arkady Arkhangorodsky <arkady.arkhangorodsky@mail.utoronto.ca>
2015-01-17 16:29:33 -0500

    Set up simple REST API with restful. Trouble connecting to the db though (does not respond to pings)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'),
-  restful = require('node-restful'),
-  bodyParser = require('body-parser'),
-  mongoose = restful.mongoose;
+    restful = require('node-restful'),
+    bodyParser = require('body-parser'),
+    mongoose = restful.mongoose;
 
 var app = express();
 
@@ -14,12 +14,36 @@ app.use(express.query());
 
 mongoose.connect('mongodb://admin:bounce@ec2-54-165-238-193.compute-1.amazonaws.com:27017/bounce');
 
-var Resource = app.resource = restful.model('resource', mongoose.Schema({
-  title: 'string',
-  year: 'number',
-}))
+var Schema = mongoose.Schema;
+var ItemSchema = new Schema({
+    name: String,
+    description: String,
+    img_url: String
+});
+var item = app.item = restful.model('item', ItemSchema)
 .methods(['get', 'post', 'put', 'delete']);
+item.register(app, '/items');
+// Function to get every item in the database
+item.route('all.get', function(req, res, next) {
+    res.send('Get all items');
+});
 
-Resource.register(app, '/resources');
+var LenderSchema = new Schema({
+    name: String,
+    description: String,
+    reputation: Number
+});
+var lender = app.lender = restful.model('lender', LenderSchema)
+.methods(['get', 'post', 'put', 'delete']);
+lender.register(app, '/lenders');
+
+var BorrowerSchema = new Schema({
+    name: String,
+    description: String,
+    reputation: Number
+});
+var borrower = app.borrower = restful.model('borrower', BorrowerSchema)
+.methods(['get', 'post', 'put', 'delete']);
+borrower.register(app, '/borrowers');
 
 app.listen(3000);
