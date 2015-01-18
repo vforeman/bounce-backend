da93f99f3148e1d5c9c8703ca30412bb0510b47c
victor foreman <vforeman@scs.howard.edu>
2015-01-17 21:20:30 -0500

    enable cors

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ var express = require('express'),
 
 //EXPRESS SERVER CONFIGURATION
 var app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
